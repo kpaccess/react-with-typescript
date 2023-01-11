f6e9ed710351a3a7b657f7c9b024acfe85f814d0
Krishna pradhan <krishnapradhan@Krishnas-MacBook-Pro.local>
2023-01-11 12:43:49 -0500

    fix: added pizza css

--- src/components/Pizza.tsx
+++ src/components/Pizza.tsx
@@ -1,24 +1,25 @@
-import React from 'react'
+import React from "react";
+import PizzaCss from "./Pizza.module.css";
 
 interface Pizza {
-    id: number,
-    name: string,
-    description: string,
-    price: number
+  id: number;
+  name: string;
+  description: string;
+  price: number;
 }
 
 interface Props {
-    pizza: Pizza
+  pizza: Pizza;
 }
 
 const Pizza: React.FC<Props> = ({ pizza }) => {
   return (
-    <li>
-        <h2>{pizza.name}</h2>
-        <p>{pizza.description}</p>
-        <h2>{pizza.price}</h2>        
+    <li className={PizzaCss.container}>
+      <h2>{pizza.name}</h2>
+      <p>{pizza.description}</p>
+      <h2>{pizza.price}</h2>
     </li>
-  )
-}
+  );
+};
 
-export default Pizza
+export default Pizza;
